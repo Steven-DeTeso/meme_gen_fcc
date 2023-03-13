d1fce1e2f750ac0472d1b0e91107094ec3c3efb8
Steven DeTeso <sadeteso@gmail.com>
2023-03-13 17:30:18 -0400

    meme component created

---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Meme () {
+    return (
+        <div className="meme-container">
+            <form>
+                <input className="meme-input"></input>
+                <input className="meme-input"></input>
+                <button className="meme-btn">Get a new meme image  🖼</button>
+            </form>
+        </div>
+    )
+}
+
+export default Meme
