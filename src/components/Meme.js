96083d404fb49dea132243ddf1f9f5179e2e8b03
Steven DeTeso <sadeteso@gmail.com>
2023-03-14 14:03:10 -0400

    linked random url to img tag

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,22 @@
 import React from "react";
+import memesData from "../memesData";
 
 function Meme () {
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
     return (
         <div className="meme-container">
-            <form>
+            <div>
                 <input className="meme-input" type="text"/>
                 <input className="meme-input" type="text"/>
-                <button className="meme-btn">Get a new meme image  🖼</button>
-            </form>
+                <button className="meme-btn" onClick={getMemeImage}>Get a new meme image  🖼</button>
+            </div>
+            <img className="meme-img" src={`${memeImage}`} alt="meme img" />
         </div>
     )
 }
